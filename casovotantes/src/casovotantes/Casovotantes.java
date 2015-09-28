cda168001d6ccdf841e685c58ef2fae59525773b
yojvn <yojvn@DESKTOP-P6N3GPQ>
2015-09-28 12:28:08 -0300

    hola

---
+++ casovotantes/src/casovotantes/Casovotantes.java
@@ -0,0 +1,19 @@
+package casovotantes;
+
+public class Casovotantes {
+    public static void main(String[] args) {
+
+      int n = (int)Math.floor(Math.random()*(100-500)+500);
+      int votantes [][] = new int [2] [n];  
+      for(int i=0; i<n;i++){
+          for(int j=0;j<2;j++){
+              
+              
+          }
+          
+      }
+      System.out.println("vamos bien creo :v " +n);
+        
+    }
+    
+}
